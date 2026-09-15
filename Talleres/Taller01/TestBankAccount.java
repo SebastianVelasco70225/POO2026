3188e393497ac95a01a3f0daeb78a15d56001256
SebastianVelasco70225 <sebastian.velasco70225@ucaldas.edu.co>
2026-09-15 11:36:27 -0500

    Refactor TestBankAccount to use comprobation method

--- Talleres/Taller01/TestBankAccount.java
+++ Talleres/Taller01/TestBankAccount.java
@@ -3,14 +3,30 @@ package Talleres.Taller01;
 public class TestBankAccount {
     public static void main(String[] args) {
         BankAccount bankAccount= new BankAccount();
-        bankAccount.setAccountHolder("Sebastián");
-        bankAccount.setAccountNumber(70225);
-        bankAccount.setAccountNumber(2);
-        bankAccount.deposits(5000);
-        bankAccount.withdrawals(2000);
-        bankAccount.withdrawals(-3000);
-        bankAccount.deposits(-100);
-        bankAccount.withdrawals(9000);
+        boolean valid=bankAccount.setAccountHolder("Sebastián");
+        comprobation(valid);
+        valid=bankAccount.setAccountNumber(70225);
+        comprobation(valid);
+        valid=bankAccount.setAccountNumber(2);
+        comprobation(valid);
+        valid=bankAccount.deposits(5000);
+        comprobation(valid);
+        valid=bankAccount.withdrawals(2000);
+        comprobation(valid);
+        valid=bankAccount.withdrawals(-3000);
+        comprobation(valid);
+        valid=bankAccount.deposits(-100);
+        comprobation(valid);
+        valid=bankAccount.withdrawals(9000);
+        comprobation(valid);
         System.out.println("El saldo de la cuenta es: "+bankAccount.getBalance());
     }
+    public static void comprobation(boolean valid){
+                if(valid){
+                        System.out.println("Información actualizada con éxito");
+                }
+                else{
+                        System.out.println("Valor inválido ingresado");
+                }
+        }
 }
