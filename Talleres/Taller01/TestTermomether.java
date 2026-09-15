ac155bb8917d10bc94169847387be61ad95b467a
SebastianVelasco70225 <sebastian.velasco70225@ucaldas.edu.co>
2026-09-15 11:07:54 -0500

    Update TestTermomether to validate temperature settings
    Refactor temperature setting logic to include validation and output.

--- Talleres/Taller01/TestTermomether.java
+++ Talleres/Taller01/TestTermomether.java
@@ -3,13 +3,19 @@ package Talleres.Taller01;
 public class TestTermomether {
     public static void main(String[] args) {
         Thermometer termomether= new Thermometer();
-        termomether.setTemperature(20);
-        termomether.getTemperature();
-        termomether.setTemperature(-40);
-        termomether.getTemperature();
-        termomether.setTemperature(180);
-        termomether.getTemperature();
-        termomether.setTemperature(-180);
-        termomether.getTemperature();
+        boolean validTemperature=termomether.setTemperature(20);
+        comprobation(validTemperature,20);
+        validTemperature=termomether.setTemperature(200);
+        comprobation(validTemperature, 200);
+        validTemperature=termomether.setTemperature(-200);
+        comprobation(validTemperature, -200);
     }
+    public static void comprobation(boolean validTemperature, float temperature){
+        if(validTemperature){
+            System.out.println("Temperatura actualizada a: "+temperature);
+        }
+        else{
+            System.out.println("No fue posible modificar la temperatura");
+    }
+}
 }
