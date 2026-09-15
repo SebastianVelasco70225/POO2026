51f61c8ce370ff938a6cc94b71dd9c65b97b5f7a
SebastianVelasco70225 <sebastian.velasco70225@ucaldas.edu.co>
2026-09-15 11:13:47 -0500

    Change setter methods to return boolean values

--- Talleres/Taller01/RoomReservation.java
+++ Talleres/Taller01/RoomReservation.java
@@ -7,46 +7,50 @@ public class RoomReservation {
     private int nightPrice;
     private int totalCost;
 
-    public void setGuestName (String guestName){
+    public boolean setGuestName (String guestName){
         if(guestName!=""||guestName!=null){
             this.guestName=guestName;
+            return true;
         }
         else{
-            System.out.println("El nombre de huésped no pudo actualizarse");
+             return false;
         }
     }
 
-    public void setRoomNumber(int roomNumber){
+    public boolean setRoomNumber(int roomNumber){
         if (roomNumber>0){
             this.roomNumber=roomNumber;
+            return true;
         }
         else {
-            System.out.println("Cuarto inválido");
+            return false;
         }
     }
 
-    public void setNightNumber(int nightNumber){
+    public boolean setNightNumber(int nightNumber){
         if (nightNumber>0){
             this.nightNumber=nightNumber;
             totalCost=this.nightNumber*nightPrice;
+            return true;
         }
         else{
-            System.out.println("Número de noches inválido");
+            return false;
         }
     }
 
-    public void setNightPrice(int nightPrice){
+    public boolean setNightPrice(int nightPrice){
         if(nightPrice>0){
             this.nightPrice=nightPrice;
             totalCost=nightNumber*this.nightPrice;
+            return true;
         }
         else {
-            System.out.println("Precio por noche inválido");
+            return false;
         }
     }
 
-    public void getTotalCost(){
-        System.out.println(totalCost);
+    public int getTotalCost(){
+        return totalCost;
     }
 
     public void getInfo(){
